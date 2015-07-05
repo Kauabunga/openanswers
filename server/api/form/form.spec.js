cc525a62cae219273ed673683eacd93922bec2ac
Carson Bruce <carson.bruce@solnet.co.nz>
2015-07-05 14:15:02 +1200

    controller test evaluates dynamic js form

--- server/api/form/form.spec.js
+++ server/api/form/form.spec.js
@@ -45,17 +45,40 @@ describe('GET /api/forms', function() {
 
   });
 
-  xit('should respond with form definition with a version', function(done) {
+  it('should respond with form definition with a version', function(done) {
 
     setTimeout(function(){
       request(app)
         .get('/api/forms/testForm/1.0.0')
         .expect(200)
-        .expect('Content-Type', /json/)
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
         .end(function(err, res) {
-          if (err) return done(err);
+          if (err) {return done(err)};
           res.body.should.be.instanceof(Object);
-          done();
+
+
+          var script = res.text;
+
+          //TODO will quite possibly need to stub out angular services
+          var window = {
+            openanswersCallback: function(formDefinition, formlyTypes){
+              //console.log('eval callback formDefinition', formDefinition);
+              //console.log('eval callback formlyTypes', formlyTypes);
+
+              formDefinition.should.be.instanceof(Object);
+              formlyTypes.should.be.instanceof(Object);
+
+              done();
+            }
+          };
+
+          try {
+            eval(script);
+          }
+          catch(err){
+            console.log('error evaluating script', err);
+          }
+
         });
     }, 1000);
 
